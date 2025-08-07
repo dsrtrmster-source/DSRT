965c66fedcf9d43279d203e8e5287542c65ab03f
dsrtrmster-source <dsrtrmster@gmail.com>
2025-08-08 06:14:50 +0700

    Update next.config.js

--- App/Landing/next.config.js
+++ App/Landing/next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  swcMinify: false,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: false, // Matikan SWC minify
+  compiler: {},     // Kosongkan agar SWC tidak dipakai
+  experimental: {
+    forceSwcTransforms: false // Matikan semua SWC transform
+  }
 };
+
+module.exports = nextConfig;
