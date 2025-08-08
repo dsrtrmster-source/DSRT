e81c17b08108b77f0ccb56e43b8abc4c15e01160
dsrtrmster-source <dsrtrmster@gmail.com>
2025-08-08 07:41:55 +0700

    Update and rename index to index.tsx

---
+++ App/Auth/index.tsx
@@ -0,0 +1,9 @@
+// App/Auth/index.tsx
+export default function AuthPage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Auth Module</h1>
+      <p>Silakan pilih menu login atau register.</p>
+    </div>
+  );
+}
