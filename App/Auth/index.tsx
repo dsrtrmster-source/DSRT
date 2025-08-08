da3ee9ca2646f8d44914b2545c24b26403750b6b
dsrtrmster-source <dsrtrmster@gmail.com>
2025-08-08 07:46:55 +0700

    Delete App/Auth/index.tsx

--- App/Auth/index.tsx
+++
@@ -1,9 +0,0 @@
-// App/Auth/index.tsx
-export default function AuthPage() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Auth Module</h1>
-      <p>Silakan pilih menu login atau register.</p>
-    </div>
-  );
-}
